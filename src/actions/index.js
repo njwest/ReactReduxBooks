0ce1bcaa46563d8c9be09bf8c5f69401794f15e1
njwest <njwest9@gmail.com>
2017-01-02 17:10:36 -0500

    action reducer completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ export function selectBook(book){
   // selectBook is an ActionCreator that must return an action
   // an object with a "type" property. "type:"" is always upper case
   return{
-    type: 'BOOK_SELECTED'
+    type: 'BOOK_SELECTED',
     payload: book
   };
 }
