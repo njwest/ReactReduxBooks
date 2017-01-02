d10bbf4913975db9906972185462768e6c16c7fe
njwest <njwest9@gmail.com>
2017-01-02 17:47:14 -0500

    booklist completed, actions wired up and pages: added to book objects

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function(){
   return[
-    {title: 'Slappy Potter'},
-    {title: 'JavaStrips'},
-    {title: 'Lurker at the Threshold'},
-    {title: 'Occult Apps'}
+    {title: 'Slappy Potter', pages: 101},
+    {title: 'JavaStrips', pages: 777},
+    {title: 'Lurker at the Threshold', pages: 214},
+    {title: 'Occult Apps', pages: 666}
   ]
 }
