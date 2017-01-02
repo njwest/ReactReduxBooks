82d7518ed2191c41da604942dbed9a4af5d2451d
njwest <njwest9@gmail.com>
2017-01-02 14:07:25 -0500

    action added to book-list list items

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { selectBook } from '../action/index';
+import { selectBook } from '../actions/index';
 import { bindActionCreators } from 'redux';
 
 class BookList extends Component{
   renderList(){
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">
+          {book.title}
+        </li>
       )
     })
   }
